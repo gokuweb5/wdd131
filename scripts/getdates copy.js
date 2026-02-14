c53c22058d656ab11ed641be097c88c1c99533d0
gokuweb5 <lnavarrete1@byupathway.edu>
2026-02-13 21:18:40 -0600

    W05 Review Products

---
+++ scripts/getdates copy.js
@@ -0,0 +1,5 @@
+// Set the current year in the footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Set the last modified date
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
